2d5dc597a4c5d96e66884191e3548210e57afb91
ilabadmin <ilabadmin@asf11-2.local>
2016-09-30 19:06:50 +0300

    initial xcode setup

---
+++ code/SMSScheduler/SMS Scheduler/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  SMS Scheduler
+//
+
+//Created by ilabafrica on 24/08/2016.
+// Copyright © 2016 Strathmore. All rights reserved.
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) NSDictionary *smsInfo;
+
+@end
+
